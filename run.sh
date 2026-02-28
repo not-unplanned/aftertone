e08b35a1f7fc9df1b8998e03811e9a309c00768a
Steve Powell <not.unplanned@gmail.com>
2026-02-28 06:26:05 +0000

    update path to mimic serving from githib.io

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #/bin/bash 
 
-(sleep 3 && open http://localhost:8000 ) &
-python3 -m http.server 8000
+cd $(dirname $0)/..
+(sleep 3 && open http://localhost:8000/aftertone/index.html ) &
+python3 -m http.server 8000 
 
