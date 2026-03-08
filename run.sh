ade1752c51a7f9a4b6df883c0ac6dca923b1968c
Steve Powell <not.unplanned@gmail.com>
2026-03-08 20:22:56 +0000

    include debug flag for local run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash 
 
 cd $(dirname $0)/..
-(sleep 3 && open http://localhost:8000/aftertone/index.html ) &
+(sleep 3 && open http://localhost:8000/aftertone/index.html?debug ) &
 python3 -m http.server 8000 
 
